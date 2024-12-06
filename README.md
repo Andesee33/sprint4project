b802c3e9ccfbb85117a4c9ee659fcffcdef868cf
Ande Cedeno <andecedeno@Andes-MacBook-Pro.local>
2024-12-06 12:48:23 -0500

    I have downlloaded a dataset and conducted some EDA. All of the EDA can be found in the eda notebook. I then created an app python file , so that my app can run. Lastly a streamlit folder that will help run the app via render

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sprint4project
+# This project is an exercise to test and apply software develoment, lessons and working with repositories in the terminal. The app itself is a representation of simple visual elements lifted from a dataset.
+# If interested in using this as a template you can refer to the 'app.py' file and replace the dataframes with your own and it will work.
---
+++ app.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+df_vehicles = pd.read_csv("/Users/andecedeno/sprint4project2-1/vehicles_us.csv")
+def extract_manufacturer(model_name):
+    # Splitting by space and assuming the first word is the manufacturer
+    return model_name.split()[0].lower()  # Assuming all manufacturer names are lowercase
+
+# Apply the function to create the 'manufacturer' column
+df_vehicles['manufacturer'] = df_vehicles['model'].apply(extract_manufacturer)
+manu_adv = df_vehicles.groupby('manufacturer')['date_posted'].count().reset_index() 
+manu_years_df = df_vehicles.groupby('manufacturer')['model_year'].mean().reset_index()
+manu_price_df = df_vehicles.groupby('manufacturer')['price'].mean().reset_index() 
+st.header("Car Advertising at a glance",divider="red")
+st.header("Car price per Manufcturer", divider="red")
+fig = px.histogram(manu_price_df,x='price',color='manufacturer',title='Average price by Manufacturer')
+st.plotly_chart(fig)
+st.header("Model Year per manufacturer", divider="red")
+fig = px.scatter(manu_years_df,x='manufacturer',color='model_year',title='Average Model Year by manufacturer')
+st.plotly_chart(fig)
+st.header("Advertisements created per manufacturer", divider="red")
+fig = px.histogram(manu_adv,x='date_posted',color='manufacturer',title='Ads posted by manufacturer')
+st.plotly_chart(fig)
+manu_model = df_vehicles.groupby(['manufacturer','model'])['date_posted'].count().reset_index()
+st.header("Advertisements per manufacturer and model",divider="red")
+option= st.selectbox("Choose a manufacturer",("acura","bmw",
+"buick","cadillac","chevrolet","chrysler","dodge","ford","gmc","honda","hyundai","jeep","kia","nissan","ram","subaru","toyota")
+)
+st.write(option)
+st.bar_chart(manu_model[manu_model['manufacturer']== option] ,x="model",y="date_posted")
